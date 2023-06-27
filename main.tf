b73515e5352dbeeb0fa01101d4b184348c81c59a
Romain TORTOSA <romain.tortosa@gmail.com>
2023-06-27 17:08:20 +0100

    script déploy kvm

--- main.tf
+++ main.tf
@@ -1,41 +1,48 @@
 terraform {
   required_providers {
     libvirt = {
-      source = "dmacvicar/libvirt"
+      source = "dmacvicar/libvirt"  # Fournisseur Libvirt provenant de la source "dmacvicar/libvirt"
     }
-  }
-}
-provider "libvirt" {
-    uri = "qemu:///system"
-}
-resource "libvirt_volume" "ubuntu_focal_server" {
-  name   = "zabbix.qcow2" 
-  format = "qcow2"
-  source = "/home/romain/Téléchargements/ubuntu-22.04.2-desktop-amd64.iso"
-}
-resource "libvirt_volume" "test_ubuntu" {
-    name           = "test_ubuntu.qcow2"
-    size = 10000000000
-}
-resource "libvirt_domain" "default" {
-    name = "testvm-ubuntu1"
-    vcpu = 2
-    memory = 2048
-    running = false 
-    disk {
-      volume_id = libvirt_volume.ubuntu_focal_server.id
-    }
-    disk {
-      volume_id = libvirt_volume.test_ubuntu.id
-    }
-    network_interface {
-      bridge = "virbr0"
-    }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent"
-    ]
   }
 }
 
+provider "libvirt" {
+    uri = "qemu:///system"  # URI de connexion au système QEMU/KVM local
+}
+
+resource "libvirt_volume" "ubuntu_focal_server" {
+  name   = "zabbix.qcow2"  # Nom du volume
+  format = "qcow2"  # Format du volume
+  source = "/home/romain.tortosa@Digital-Grenoble.local/snap/firefox/common/Downloads/ubuntu-22.04.2-desktop-amd64.iso"  # Chemin d'accès du fichier ISO d'installation d'Ubuntu
+}
+
+resource "libvirt_volume" "test_ubuntu" {
+    name   = "test_ubuntu.qcow2"  # Nom du volume
+    size = 10000000000  # Taille du volume en octets (10 Go)
+}
+
+resource "libvirt_domain" "default" {
+    name   = "testvm-ubuntu1"  # Nom de la machine virtuelle
+    vcpu   = 2  # Nombre de VCPUs
+    memory = 2048  # Mémoire en Mo
+    running = false  # La machine virtuelle ne sera pas démarrée automatiquement après sa création
+
+    disk {
+      volume_id = libvirt_volume.ubuntu_focal_server.id  # Associe le volume "ubuntu_focal_server" à la machine virtuelle
+    }
+
+    disk {
+      volume_id = libvirt_volume.test_ubuntu.id  # Associe le volume "test_ubuntu" à la machine virtuelle
+    }
+
+    network_interface {
+      bridge = "virbr0"  # Interface réseau utilisant le pont "virbr0"
+    }
+
+    provisioner "remote-exec" {
+      inline = [
+        "sudo apt-get update",
+        "sudo apt-get install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent"  # Commandes exécutées à distance pour l'installation de Zabbix
+      ]
+    }
+}
